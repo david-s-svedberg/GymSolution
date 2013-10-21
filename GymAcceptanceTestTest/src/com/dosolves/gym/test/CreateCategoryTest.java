1762fd043e221fd56b423fd7458d4ea4a55a8a13
David <david.s.svedberg@gmail.com>
2013-10-21 09:36:30 +0200

    Added acceptancetest for CreateCategory UseCase

---
+++ GymAcceptanceTestTest/src/com/dosolves/gym/test/CreateCategoryTest.java
@@ -0,0 +1,39 @@
+package com.dosolves.gym.test;
+
+import android.test.ActivityInstrumentationTestCase2;
+import android.test.suitebuilder.annotation.MediumTest;
+import android.test.suitebuilder.annotation.SmallTest;
+
+import com.dosolves.gym.CategoryActivity;
+import com.dosolves.gym.R;
+
+public class CreateCategoryTest extends ActivityInstrumentationTestCase2<CategoryActivity>{
+
+	private CategoryActivity activityUnderTest;
+	
+	public CreateCategoryTest(){
+		super(CategoryActivity.class);
+	}
+	
+	public CreateCategoryTest(Class<CategoryActivity> activityClass) {
+		super(activityClass);
+	}
+	
+	@Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        activityUnderTest = getActivity();     
+    }
+	
+	@SmallTest
+	public void testPreconditions() {
+	    assertNotNull(activityUnderTest);	    
+	}
+	
+	@MediumTest
+	public void test_Adding_new_category_shoud_add_category_to_list(){
+		boolean menuItemFound = getInstrumentation().invokeMenuActionSync(activityUnderTest,R.menu.category , 0);
+		assertTrue("Menuitem 'Add category' not found",menuItemFound);
+	}
+
+}
